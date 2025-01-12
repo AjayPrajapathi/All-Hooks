ef421137b0bc334457395c388c9aafc62163e721
AjayPrajapathi <ajayprajapathi4@gmail.com>
2025-01-12 18:07:41 +0530

    file structure is created

---
+++ hooks/src/BasicHooks/taskDetails.jsx
@@ -0,0 +1,21 @@
+// 1. Basic Hooks
+// These hooks form the foundation of React functionality.
+
+// Hooks:
+// useState: For managing local state in functional components.
+// useEffect: For performing side effects like fetching data, subscribing to events, or updating the DOM.
+// useContext: For accessing global state using the Context API.
+// Task: Weather Dashboard
+// Create a weather dashboard that allows users to:
+// Search for a city (use useState for the input field and the fetched weather data).
+// Display weather details (e.g., temperature, humidity, and wind speed).
+// Use useEffect to fetch data from a weather API (e.g., OpenWeather) whenever the user searches for a new city.
+// Use useContext to manage user preferences like temperature units (Celsius or Fahrenheit) and apply them across components.
+
+
+
+
+
+
+
+
